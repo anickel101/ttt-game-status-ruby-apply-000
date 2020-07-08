12091beabc55f4f069814f8a6bcf7e19d8a87b07
Anson Nickel <anson.nickel@gmail.com>
2020-07-08 20:28:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,17 +50,12 @@ def over?(board)
   end
 end
 
-def winner?(board)
-  if won?(board)
-    return board[won?(board)[0]]
-  end
-end
-
 def winner(board)
   if won?(board)
     return board[won?(board)[0]]
   end
 end
+
 
     
     
