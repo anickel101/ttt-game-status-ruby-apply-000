3c3b4e3960564f4f7ce9f8300e10f669bc99602d
Anson Nickel <anson.nickel@gmail.com>
2020-07-08 20:28:52 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,60 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],  # top row
+  [3,4,5],  # middle row
+  [6,7,8],  # bottom row
+  [0,3,6],  # left col
+  [1,4,7],  # center col
+  [2,5,8],  # right col
+  [0,4,8],  # left diag
+  [2,4,6]   # right diag
+  ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combos|
+   
+    test_square1 = combos[0]
+    test_square2 = combos[1]
+    test_square3 = combos[2]
+ 
+    actual_square1 = board[test_square1]
+    actual_square2 = board[test_square2]
+    actual_square3 = board[test_square3]
+    
+    if actual_square1 == actual_square2 && actual_square2 == actual_square3 && position_taken?(board, test_square1)
+      return combos
+    end
+  end
+  return false
+end
+
+def full?(board)
+  !board.any? {|index| index == " "}
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  elsif !full?(board) && !won?(board)
+    return false
+  end
+end
+
+def over?(board)
+  if full?(board) || won?(board) || draw?(board)
+    return true
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
+
+
+    
+    
+  
